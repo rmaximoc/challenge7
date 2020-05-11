f0c43d9ebe1e46bebdfa1a07589017746d18521e
Rodrigo <rodrigomaximocano@gmail.com>
2020-05-11 18:48:44 -0300

    Add listing from API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import api from '../../services/api';
 import Header from '../../components/Header';
 
 import formatValue from '../../utils/formatValue';
+import toCapitalCase from '../../utils/toCapitalCase';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -30,12 +31,14 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const { data } = await api.get('/transactions');
+      setTransactions(data.transactions);
+      setBalance(data.balance);
     }
 
     loadTransactions();
@@ -51,21 +54,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{`R$ ${balance.income}`}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{`R$ ${balance.outcome}`}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{`R$ ${balance.total}`}</h1>
           </Card>
         </CardContainer>
 
@@ -76,23 +79,19 @@ const Dashboard: React.FC = () => {
                 <th>Título</th>
                 <th>Preço</th>
                 <th>Categoria</th>
-                <th>Data</th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {`R$ ${transaction.value}`}
+                  </td>
+                  <td>{toCapitalCase(transaction.type)}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
---
+++ src/utils/toCapitalCase.ts
@@ -0,0 +1,4 @@
+const toCapitalCase = (text: string): string =>
+  text.charAt(0).toUpperCase() + text.slice(1);
+
+export default toCapitalCase;
