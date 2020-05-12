c76a12c0c542dedb94f0fea1d65fe63be6199166
Rodrigo <rodrigomaximocano@gmail.com>
2020-05-11 22:58:16 -0300

    Add links to dashboard

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -22,6 +22,7 @@ export const Container = styled.div<ContainerProps>`
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
+        border: opacity 0.2s;
 
         & + a {
           margin-left: 32px;
@@ -29,6 +30,7 @@ export const Container = styled.div<ContainerProps>`
 
         &:hover {
           opacity: 0.6;
+          border-bottom: 2px solid #ff872c;
         }
       }
     }
